ce14d751b0bf8a6e3e02cc9137ad46f608e80eb9
Godti Vinod <venkat.godti3@gmail.com>
2020-10-28 03:00:16 +0530

    Display game name and score list from session storage

--- src/components/scoreCard/index.js
+++ src/components/scoreCard/index.js
@@ -1,15 +1,35 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import './score-card.scss'
-class ScoreCard extends Component{
-    render(){
-        return(
+class ScoreCard extends Component {
+    state = {
+        scoreBoard: []
+    }
+    componentDidMount() {
+        let scoreBoard = window.sessionStorage.getItem('scoreBoard');
+        scoreBoard = scoreBoard ? JSON.parse(scoreBoard) : [];
+        const personalBest = scoreBoard && scoreBoard.length > 0 &&
+            scoreBoard.reduce((acc, cur) => acc.score > cur.score ? acc : cur, {});
+        this.setState({ scoreBoard, personalBest });
+    }
+    render() {
+        let { scoreBoard, personalBest } = this.state;
+        return (
             <div className="score-card-container">
                 <h4>Score Board</h4>
-                <p>Game1 : 1:14</p>
-                <p>Game2 : 1:27</p>
-                <p>Game3 : 2.01</p>
-                <h5>Personal Best</h5>
-                <p>Game4 : 2.25</p>
+                {scoreBoard && scoreBoard.length === 0 && <p>Your score board is empty!</p>}
+                {scoreBoard && scoreBoard.length > 0 &&
+                    scoreBoard.map((scoreData, index) => {
+                        return index < 5 ?
+                            <p key={scoreData.name}>{scoreData.name} : {scoreData.formattedScore}</p>
+                            : ""
+                    })
+                }
+                {personalBest &&
+                    <div>
+                        <h5>Personal Best</h5>
+                        <p>{personalBest.name} : {personalBest.formattedScore}</p>
+                    </div>
+                }
             </div>
         )
     }
