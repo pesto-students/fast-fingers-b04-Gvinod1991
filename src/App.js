33860bea4b44bcdf46fba5b0c0939fd42bf4ee3d
Godti Vinod <venkat.godti3@gmail.com>
2020-10-27 10:40:03 +0530

    Render play game screen and header in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
-
+// import Logo from  './components/logo';
+// import StartGame from './screens/start-game'
+import Header from './components/header'
+import PlayGame from './screens/play-game'
 function App() {
   return (
     <div className="App">
-      <h1>Fast Fingers </h1>
+      {/* <Logo /> */}
+      {/* <StartGame/> */}
+      <Header/>
+      <PlayGame/>
     </div>
   );
 }
