50e6b93ce2c3b3d50aff0ae43e7ba2e4a6a0967d
Godti Vinod <venkat.godti3@gmail.com>
2020-10-29 18:46:11 +0530

    UI touchup and code review fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -11,17 +11,17 @@ class Header extends Component{
         this.setState({playerName})
     }
     render(){
-        let {score,gameLevel}=this.props;
-        let {playerName}=this.state;
+        const {score,gameLevel}=this.props;
+        const {playerName}=this.state;
         return(
             <div className="container-fluid header-container">
                 <div className="row custom-row">
-                    <h2 className="col-md-4 col-sm-6 player-name"><img src={playerIcon} alt="player icon"/>{playerName}</h2>
-                    <h2 className="offset-md-4 col-md-3 col-sm-6 game-name">fast fingers</h2>
+                    <h2 className="col-md-4 col-12 player-name"><img src={playerIcon} alt="player icon"/>{playerName}</h2>
+                    <h2 className="offset-md-3 col-md-4 col-12 game-name">fast fingers</h2>
                 </div>
                 <div className="row">
-                    <h2 className="col-md-5 game-level"><img src={gamePadIcon}  alt="game pad icon"/>LEVEL : {gameLevel}</h2>
-                    <h2 className="offset-md-3 col-md-3 score">SCORE: {score}</h2>
+                    <h2 className="col-md-5 col-12 game-level"><img src={gamePadIcon}  alt="game pad icon"/>LEVEL : {gameLevel}</h2>
+                    <h2 className="offset-md-3 col-md-3 col-12 score">SCORE: {score}</h2>
                 </div>
             </div>
         )
