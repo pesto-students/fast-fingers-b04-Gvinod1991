4004aed88594e182bc57927baa8df52ccd89b99f
Godti Vinod <venkat.godti3@gmail.com>
2020-10-29 18:48:54 +0530

    Code lint fix

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,12 +1,12 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import './logo.scss'
 import keyboardIcon from '../../assets/images/keyboard-icon.svg'
-class Logo extends Component{
-    render(){
-        return(
+class Logo extends Component {
+    render() {
+        return (
             <div className="logo-container">
                 <img src={keyboardIcon} alt="keyboard-icon" />
-                <h2 >fast fingers</h2>
+                <h2>fast fingers</h2>
                 <p><span></span>the ultimate typing game<span></span></p>
             </div>
         )
